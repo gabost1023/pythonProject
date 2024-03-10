ce70aa5a1354e232543be930c6f4492331c73e7d
gabost1023 <161207635+gabost1023@users.noreply.github.com>
2024-03-10 12:30:26 +0100

    lab3

---
+++ LAB3_cw5.py
@@ -0,0 +1,5 @@
+def poleTrapezu(a=9,b=4,h=12):
+    pole = ((a+b)*h)/2
+    return pole
+
+print(poleTrapezu())
--- plik2.py
+++ plik2.py
@@ -77,26 +77,3 @@ import math
 # with open('pliczek.txt', 'r') as f:
 #     lines = f.readlines()
 # print(lines)
-
-def czytrojkat(a, b, c):
-    if max(a, b, c) == a:
-        if b**2 + c**2 == a**2:
-            print('Trojkat jest prostokatny')
-        else:
-            print('Trojkat nie jest prostokatny')
-    elif max(a, b, c) ==b:
-        if a**2 + c**2 == b**2:
-            print('Trojkat jest prostokatny')
-        else:
-            print('Trojkat nie jest prostokatny')
-    elif max(a, b, c) == c:
-        if a**2 + b**2 == c**2:
-            print('Trojkat jest prostokatny')
-        else:
-            print('Trojkat nie jest prostokatny')
-
-liczba1 = int(input('Podaj bok a: '))
-liczba2 = int(input('Podaj bok b: '))
-liczba3 = int(input('Podaj bok c: '))
-
-print(czytrojkat(liczba1, liczba2, liczba3))
