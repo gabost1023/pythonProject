1a062cc08c72af11749b1a272349e33dc8307c6e
gabost1023 <161207635+gabost1023@users.noreply.github.com>
2024-03-24 12:33:18 +0100

    lab4

--- plik2.py
+++ plik2.py
@@ -77,3 +77,10 @@ import math
 # with open('pliczek.txt', 'r') as f:
 #     lines = f.readlines()
 # print(lines)
+
+import random
+
+lista1 = [random.randint(0, 120) for i in range(10)]
+print(lista1)
+lista2 = [x for x in lista1 if x%2==0]
+print(lista2)
